82cb32ecabcdebe6069e3f88a8a91d3c52093a8a
Eurico Inocencio <eurico.inocencio@gmail.com>
2022-08-22 16:08:53 +0100

    minimum standalone version

--- casper-epaper-lit.js
+++ casper-epaper-lit.js
@@ -5,9 +5,10 @@
  -
  */
 
+
 // https://marian-caikovski.medium.com/do-not-waste-users-time-on-rendering-off-screen-content-eed17636e7a7
 
-import { html, css } from 'lit';
+import { html, css, LitElement } from 'lit';
 
 class EpaperRenderer {
   static get BOLD_MASK ()       { return 0x01; }
@@ -304,24 +305,69 @@ class EpaperSvgRenderer extends EpaperRenderer {
 
 class CasperEpaperLit extends LitElement {
 
+  get MAX_ZOOM () { return 3;   }
+  get MIN_ZOOM () { return 0.5; }
+
+  static properties = {
+    zoom: {
+      type: Number,
+      reflect: true
+    }
+  }
+
   static styles = css`
 
     :host {
+      position: relative;
       display: flex;
     }
 
-    .ilfondo {
+    casper-icon-button {
+      width: 32px;
+      height: 32px;
+      margin-left: 8px;
+    }
+
+    .background {
+      position: absolute;
       background-color: #ccc;
       display: flex;
+      width: 100%;
+      height: 100%;
       flex-direction: column;
       justify-content: center;
+      overflow: auto;
+    }
+
+    .toolbar {
+      top: 0;
+      position: absolute;
+      padding: 16px;
+      display: flex;
+      width: 100%;
+      box-sizing: border-box;
+    }
+
+    .shadow {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      box-sizing: border-box;
+      pointer-events: none;
+      -moz-box-shadow:    inset 0 0 10px #00000080;
+      -webkit-box-shadow: inset 0 0 10px #00000080;
+      box-shadow:         inset 0 0 10px #00000080;
+    }
+
+
+    .page {
+      background-color: white;
+      place-self: center;
     }
 
     .ilcanvas {
-      width: 595px;
-      height: 842px;
-      background-color: white;
-      place-self: center;
+      width: 100%;
+      height: 100%;
     }
 
     .debug-margin {
@@ -340,6 +386,108 @@ class CasperEpaperLit extends LitElement {
 
   `;
 
+  constructor () {
+    super();
+    this._svgRenderer = new EpaperSvgRenderer();
+    this.zoom = 1;
+    this._pageWidth  = 595;
+    this._pageHeight = 842;
+  }
+
+  async openAttachment (attachment, attachmentIndex, controlButtonsOptions = {}) {
+    /*
+      Review all this "crappy bloat"
+
+    this.__controlButtonsOptions = controlButtonsOptions;
+
+    if (Array.isArray(attachment) && attachment.length > 0) {
+      this.__currentAttachments = attachment;
+      this.__currentAttachmentIndex = attachmentIndex !== undefined && attachmentIndex < attachment.length ? attachmentIndex : 0;
+      this.__currentAttachment = this.currentAttachment = { ...this.__currentAttachments[this.__currentAttachmentIndex] };
+    } else {
+      this.__currentAttachments = undefined;
+      this.__currentAttachmentIndex = undefined;
+      this.__currentAttachment = this.currentAttachment = { ...attachment };
+    }
+
+    this.__currentAttachmentCheckList = attachment.tasks;*/
+
+    //this.__handleAttachmentNavigationButtons();
+    //this.__openAttachment();
+    switch ( attachment.type ) {
+      case 'file/pdf':
+      case 'file/xml':
+      case 'file/txt':
+      case 'file/htm':
+      case 'file/html':
+      case 'file/email':
+      case 'file/png':
+      case 'file/jpg':
+      case 'file/jpeg':
+      case 'html':
+      default:
+        // TODO 
+        break;
+      case 'epaper':
+        return await this.openEpaper(attachment);
+    }
+  }
+
+  async __openAttachment () {
+    //this.__customAttachmentFileType = false;
+    //this.__currentAttachmentName = this.__currentAttachment.name ? this.__currentAttachment.name : '';
+    //this.__updateDownloadIconAndTooltip();
+
+    // Open the attachment.
+    /*try {
+      switch (this.__currentAttachment.type) {
+        case 'file/pdf':
+          await this.__openPDF();
+          break;
+        case 'file/xml':
+        case 'file/txt':
+        case 'file/htm':
+        case 'file/html':
+        case 'file/email':
+          this.__landscape = false;
+          await this.__openIframe();
+          break;
+        case 'file/png':
+        case 'file/jpg':
+        case 'file/jpeg':
+          this.__landscape = false;
+          await this.__openImage();
+          break;
+        case 'epaper':
+          await this.__openServerDocument();
+          break;
+        case 'html':
+          this.__landscape = this.__currentAttachment.landscape;
+          await this.__openIframe();
+          break;
+        default:
+          if (this.__currentAttachment.type.startsWith('file/')) {
+            this.__customAttachmentFileType = true;
+            this.openGenericPage(this.$['download-generic-file-template']);
+          }
+          break;
+      }
+    } catch (error) {
+      console.error(error);
+
+      //this.__displayErrorPage();
+    }*/
+  }
+
+  /**
+   * Sets the epaper's zoom to a specific value.
+   */
+  setZoom (zoom) {
+    /*if (this.zoom >= CasperEpaper.EPAPER_MIN_ZOOM && this.zoom <= CasperEpaper.EPAPER_MAX_ZOOM) {
+      this.zoom = zoom;
+    }*/
+  }
+
   //***************************************************************************************//
   //                                ~~~ LIT life cycle ~~~                                 //
   //***************************************************************************************//
@@ -347,21 +495,40 @@ class CasperEpaperLit extends LitElement {
   render () {
 
     return html`
-      <div id="ilfondo" class="ilfondo">
-        <svg id="ilsvg" class="ilcanvas">
-        </svg>
+      <div class="background">
+        <div id="page" class="page">
+          <svg id="ilsvg" class="ilcanvas">
+          </svg>
+        </div>
+        <div class="toolbar">
+          <casper-icon-button @click="${(e) => this._zoomOut(e)}"          tooltip="Reduzir"         icon="fa-light:minus" reverse></casper-icon-button>
+          <casper-icon-button @click="${(e) => this._zoomIn(e)}"           tooltip="Ampliar"         icon="fa-light:plus" reverse></casper-icon-button>
+          <casper-icon-button @click="${(e) => this._gotoPreviousPage(e)}" tooltip="Página anterior" icon="fa-light:arrow-left"></casper-icon-button>
+          <casper-icon-button @click="${(e) => this._gotoNextPage(e)}"     tooltip="Página seguinte" icon="fa-light:arrow-right"></casper-icon-button>
+        </div>
+      </div>
+      <div class="shadow">
       </div>
     `;
   }
 
+  willUpdate (changedProperties) {
+
+    if ( changedProperties.has('zoom') && this._svgRenderer._svg ) {
+      this._page.style.width  = this._pageWidth  * this.zoom + 'px';
+      this._page.style.height = this._pageHeight * this.zoom + 'px';
+    }
+  }
+
   firstUpdated (changedProperties) {
-    //this._epaper = this.shadowRoot.getElementById('epaper');
     // temporary plumbing for sandbox
-    this._svgRenderer = new EpaperSvgRenderer();
-    //this._epaper.$.serverDocument._svgRenderer = this._svgRenderer;
     this._svgRenderer._svg    = this.shadowRoot.getElementById('ilsvg');
-    this._svgRenderer._holder = this.shadowRoot.getElementById('ilfondo');
-    //this._svgRenderer._styleSheet = new CSSStyleSheet();
+
+    this._svgRenderer._holder = this.shadowRoot.getElementById('page');
+    this._page = this.shadowRoot.getElementById('page');
+    this._page.style.width  = this._pageWidth  * this.zoom + 'px';
+    this._page.style.height = this._pageHeight * this.zoom + 'px';
+
     if (this.shadowRoot.adoptedStyleSheets) {
       this._svgRenderer._styleSheet = this.shadowRoot.adoptedStyleSheets[0];
     } else {
@@ -373,6 +540,250 @@ class CasperEpaperLit extends LitElement {
   //***************************************************************************************//
   //                             ~~~ internal methods ~~~                                  //
   //***************************************************************************************//
+
+  /**
+   * Navigate to the previous page.
+   */
+  async _gotoPreviousPage () {
+    if (this._currentPage > 1) {
+      this._currentPage--;
+      if ( 2.0 === this._socket._version ) { // [AG] - HB: required on v2...
+        //await this._currentPageChanged(this._currentPage)
+        await this._socket.gotoPage(this.documentId, this._currentPage);
+      }
+    }
+  }
+
+  /**
+   * Navigate to the next page.
+   */
+  async _gotoNextPage () {
+    if ( 2.0 === this._socket._version ) { // [AG] - HB: required on v2...
+      this._currentPage++;
+      //await this._currentPageChanged(this._currentPage);
+      await this._socket.gotoPage(this.documentId, this._currentPage);
+    } else {
+      if (this._currentPage < this.__totalPageCount) {
+        this._currentPage++;
+      }
+    }
+  }
+
+  /**
+   * Decreases the epaper's zoom.
+   */
+  async _zoomOut () {
+    if ( this.zoom > this.MIN_ZOOM ) {
+      this.zoom *= 0.8;
+    }
+  }
+
+  /**
+   * Increases the epaper's zoom.
+   */
+  async _zoomIn () {
+    if ( this.zoom < this.MAX_ZOOM ) {
+      this.zoom *= 1.2;
+    }
+  }
+
+  /**
+   * Open server document
+   *
+   * @param {Object} documentModel an object that specifies the layout and data of the document
+   */
+  async openEpaper (documentModel) {
+
+    if ( documentModel.epaper2 ) { // # TODO a clean api o app to get the proper socket
+      this._socket = app.socket2;
+    } else {
+      this._socket = app.socket;
+    }
+
+    this._currentPage = 1; // # TODO goto page on open /
+    /*if ( documentModel.backgroundColor ) {
+      this._setBackground(documentModel.backgroundColor);
+    } else {
+      this._setBackground('#FFF');
+    }*/
+
+    this._prepareOpenCommand(documentModel);
+    return await this._openChapter();
+  }
+
+  /**
+   * Sanitizes the document object model, auto selects the first chapter
+   *
+   * @param {Object} documentModel the document model
+   */
+  _prepareOpenCommand (documentModel) {
+    this.document       = JSON.parse(JSON.stringify(documentModel));
+    this._chapterCount  = this.document.chapters.length;
+    this.totalPageCount = 0;
+
+    for (let idx = 0; idx < this._chapterCount; idx++) {
+      this.document.chapters[idx].locale    = this.document.chapters[idx].locale    || 'pt_PT';
+      this.document.chapters[idx].editable  = this.document.chapters[idx].editable  || false;
+      this.document.chapters[idx].pageCount = this.document.pageCount               || 1;
+      this.totalPageCount += this.document.chapters[idx].pageCount;
+    }
+    this._chapterIndex = 0;
+    this._chapter      = this.document.chapters[0];
+    this._edition      = false;
+  }
+
+  /**
+   * Opens the currently selected chapter
+   *
+   * @param {number} pageNumber page starts at 1
+   */
+   async _openChapter (pageNumber) {
+    //this.loading = true;
+
+    //this.__inputBoxDrawString = undefined;
+    //this.$.servertip.enabled = false;
+    //this.$.input.setVisible(false);
+    //this.__hideWidgets(true);
+    //this.__resetScroll();
+    this._nextPage  = pageNumber || 1;
+    //this.__openFocus = this.__chapter.editable ? (this.__nextPage > 0 ? 'start' : 'end') : 'none';
+    //this.__loading = true;
+    //this.__resetCanvasDimensions();
+
+    let response;
+
+    if (!(this._jrxml === this._chapter.jrxml && this._locale === this._chapter.locale)) {
+
+      response = await this._socket.openDocument(this._chapter);
+
+      if (response.errors !== undefined) {
+        //this.__clear();
+        //throw new Error(response.errors);
+        // #TODO new error display
+      }
+
+      this.documentId  = response.id;
+      this._socket.registerDocumentHandler(this.documentId, (message) => this._documentHandler(message));
+      this._pageWidth  = response.page.width;
+      this._pageHeight = response.page.height;
+
+      if (isNaN(this._pageHeight) || this._pageHeight < 0) {
+        this._pageHeight = 4000;
+        //this.$['canvas-container'].style.overflow = 'auto';
+      } else {
+        //this.$['canvas-container'].style.overflow = '';
+      }
+
+      //this.__rightMmargin = response.page.margins.right;
+      this._jrxml        = this._chapter.jrxml;
+      this._locale       = this._chapter.locale;
+    }
+
+    //this.landscape = this.__pageHeight < this.__pageWidth;
+    //this.__zoomChanged();
+
+    this._chapter.id = this.documentId;
+
+    response = await this._socket.loadDocument({
+      id:       this.documentId,
+      editable: this._chapter.editable,
+      limit:    this._chapter.limit,
+      path:     this._chapter.path,
+      scale:    1, // New gen can use 1 always wos this.__sx,
+      focus:    this._openFocus,
+      page:     this._nextPage
+    });
+
+    if ( response.errors !== undefined ) {
+      //this.__clear();
+      //throw new Error(response.errors);
+      // #TODO new error display
+    }
+
+    this._path    = this._chapter.path;
+    this._params  = this._chapter.params;
+    this._edition = this._chapter.editable;
+    //this.documentScale  = this.__sx;
+
+    //this.__scalePxToServer = this.__pageWidth * this.__ratio / this.__canvas.width;
+
+    //this._repaintPage();
+
+    //this.__loading = false;
+    //this.$.servertip.enabled = true;
+    //this.loading = false;
+    return true;
+  }
+
+  _documentHandler (message) {
+    switch (message[0]) {
+      case 'J':
+        console.time('parse');
+        const page = JSON.parse(message.substring(2, message.length - 1));
+        console.timeEnd('parse');
+        this._svgRenderer.renderPage(page);
+        break;
+      case 'D':
+        // Ignore V1 protocol (aka "gerber") drawing orders
+        break;
+      default:
+        console.log('TODO message: ', message);
+        break;
+    }
+  }
+
+  /**
+   * Goto to the specified page. Requests page change or if needed loads the required chapter
+   *
+   * @param {number} pageNumber the page to render
+   */
+  async _currentPageChanged (pageNumber) {
+
+    if ( this.document && this.document.chapters && this.document.chapters.length >= 1 ) {
+      let currentPage = 1;
+
+      pageNumber = parseInt(pageNumber);
+      for ( let i = 0;  i < this.document.chapters.length; i++ ) {
+        if ( pageNumber >= currentPage && pageNumber < (currentPage + this.document.chapters[i].pageCount) ) {
+          let newPageNumber;
+
+          newPageNumber = 1 + pageNumber - currentPage;
+          if ( i === this.__chapterIndex ) {
+            if ( this._chapterPageNumber !== newPageNumber ) {
+              // #TODO  this.__resetScroll();
+              await this._socket.gotoPage(this.documentId, newPageNumber);
+              return pageNumber;
+            }
+          } else {
+            this.gotoChapter(i, newPageNumber);
+            return pageNumber;
+          }
+          this._chapterPageNumber = newPageNumber;
+        }
+        currentPage += this.document.chapters[i].pageCount;
+      }
+    }
+  }
+
+  _getIconForFileType (fileType) {
+    switch (fileType) {
+      case 'epaper':
+      case 'file/pdf':
+        return 'fa-light:file-pdf';
+      case 'file/txt':
+        return 'fa-light:file-alt';
+      case 'file/xml':
+      case 'file/htm':
+      case 'file/html':
+        return 'fa-light:file-code';
+      case 'file/png':
+      case 'file/jpg':
+      case 'file/jpeg':
+        return 'fa-light:file-image';
+      default:
+        return 'fa-light:file-alt';
+    }
+  }
 
 }
 
